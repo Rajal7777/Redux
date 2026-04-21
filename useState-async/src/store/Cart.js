19fcdcddf47b97670c0d0e7132221a46065939a2
Rajal7777 <suwalrajal57@gmail.com>
2026-04-21 21:02:58 +0900

    handleling http and aync

--- useState-async/src/store/Cart.js
+++ useState-async/src/store/Cart.js
@@ -1,18 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-//createSlice
-// A)it is a function that accepts an initial state, an object of reduer functions, and a slice name and creates action and action types that correspond to the reducers and state.
-
-//1) It accepts single object parameter
-//* name = a string name fotr this slice of state, action type will use this as a prefix
-
-//*reducers
-//It is function intended to handle a specific action type.
-
-//action type will use this as a prefix
-//initial  object parameter
-//reducer function which will handle a specific action type.
-
 const cartSlice = createSlice({
   name: "cart",
 
@@ -20,12 +7,15 @@ const cartSlice = createSlice({
     items: [],
     totalQuantity: 0,
   },
-
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
+
     addItemToCart(state, action) {
       const newItem = action.payload;
 
-      //check for existing item if exists [tight hand (newItem.id) from parameter]
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
 
@@ -43,6 +33,7 @@ const cartSlice = createSlice({
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
+    
     removeItemFromCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
